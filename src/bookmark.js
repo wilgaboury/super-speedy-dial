df5395209801815f3a799c1107b1fe9ddc1869d6
wilgaboury <wgaboury@gmail.com>
2020-07-24 23:12:36 -0400

    bunch of updates

--- src/bookmark.js
+++ src/bookmark.js
@@ -3,18 +3,36 @@ import { setGridWidth } from './grid_width_hack.js';
 function Bookmark(initalVnodes) {
     let parentNode;
     let bookmarkNode;
+    let onFolderClickCallback;
 
-    setTimeout(setGridWidth, 0);
+    let isSelected = false;
 
     return {
         oninit: function(vnode) {
             parentNode = vnode.attrs.parentNode;
             bookmarkNode = vnode.attrs.bookmarkNode;
+            onFolderClickCallback = vnode.attrs.onFolderClickCallback;
         },
         view: function(vnode) {
             return m(".bookmark-container", [
-                m(".bookmark-card"),
-                m(".bookmark-title", bookmarkNode.title)
+                m(".bookmark-card", {
+                    onclick: function() {
+                        if (!(bookmarkNode.url == null)) {
+                            window.location.href = bookmarkNode.url;
+                        } else if (bookmarkNode.type == "folder") {
+                            onFolderClickCallback(bookmarkNode);
+                        }
+                    },
+                    onmousedown: function() {
+                        isSelected = true;
+                        m.redraw();
+                    },
+                    onmouseup: function() {
+                        isSelected = false;
+                        m.redraw();
+                    }
+                }),
+                m(`.bookmark-title${isSelected ? ' .selected' : ''}`, bookmarkNode.title)
             ]);
         }
     }
--- src/grid.js
+++ src/grid.js
@@ -1,8 +1,16 @@
-import Bookmark from './bookmark.js'
+import Bookmark from './bookmark.js';
+import { setGridWidth } from './grid_width_hack.js';
 
 function Grid(initialVnode) {
     let bookmarkRoot;
     let nodeStack = [];
+
+    let onFolderClickCallback = function(bookmark) {
+        nodeStack.push(bookmark);
+        m.redraw();
+    }
+
+    setTimeout(setGridWidth, 0);
 
     return {
         oninit: function() {
@@ -13,12 +21,19 @@ function Grid(initialVnode) {
             })
         },
         view: function(vnode) {
-            return m(".grid-container", nodeStack.length == 0 ? m(".empty") : nodeStack[nodeStack.length - 1].children.map(bookmark => {
-                return m(Bookmark, {
-                    parentNode: nodeStack[nodeStack.length - 1], 
-                    bookmarkNode: bookmark
-                });
-            }));
+            return [
+                
+                m('.grid-container', nodeStack.length == 0 ? m('div') : nodeStack[nodeStack.length - 1].children
+                    .filter(bookmark => bookmark.type != 'separator')
+                    .map(bookmark => {
+                        return m(Bookmark, {
+                            parentNode: nodeStack[nodeStack.length - 1], 
+                            bookmarkNode: bookmark,
+                            onFolderClickCallback: onFolderClickCallback
+                        });
+                    })
+                )
+            ];
         }
     }
 }
