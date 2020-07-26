5dcf951b2c6d63b8c982fe37c58669b892e33641
wilgaboury <wgaboury@gmail.com>
2020-07-26 12:10:52 -0400

    added file picker modal

--- src/background.js
+++ src/background.js
@@ -1,8 +1,21 @@
 import Grid from './grid.js';
+import Modal from './modal.js';
 
-let Background = {
-    view: function() {
-        return m(Grid);
+function Background() {
+    let showModal = false;
+
+    return {
+        view: function() {
+            return m('.background',
+                m('img.settings-button', {src:'icons/cog.svg', height:'25', onclick: function() { showModal = true; m.redraw() }}),
+                m(Grid),
+                showModal && m(Modal,
+                    m('.modal-content',
+                        m('input#background-input', {type: 'file'})
+                    )
+                )
+            );
+        }
     }
 };
 
