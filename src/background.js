9de36c68a262c18efd98b9f9dca53f4acc99230c
wilgaboury <wgaboury@gmail.com>
2020-07-26 12:38:43 -0400

    work on modal

--- src/background.js
+++ src/background.js
@@ -3,15 +3,48 @@ import Modal from './modal.js';
 
 function Background() {
     let showModal = false;
+    let background = null;
 
     return {
+        oninit: function() {
+            browser.storage.local.get("background").then(function(file) {
+                background = file;
+                m.redraw();
+            })
+        },
         view: function() {
-            return m('.background',
-                m('img.settings-button', {src:'icons/cog.svg', height:'25', onclick: function() { showModal = true; m.redraw() }}),
+            return m('.background', {style: background == null ? '' : `background-image: url(${background})`},
+                m('img.settings-button', {
+                    src:'icons/cog.svg', 
+                    height:'25', 
+                    onclick: function() { 
+                        showModal = true; 
+                        m.redraw() 
+                    },
+                }),
                 m(Grid),
                 showModal && m(Modal,
                     m('.modal-content',
-                        m('input#background-input', {type: 'file'})
+                        m('input#background-input', {type: 'file'}),
+                        m('.modal-button-container',
+                            m('.button', {
+                                onclick: function() {
+                                    const file = document.querySelector('#background-input').files[0];
+                                    browser.storage.local.set({
+                                        background: file
+                                    });
+                                    showModal = false;
+                                    m.redraw();
+                                }
+                            }, 'Ok'),
+                            m('.button', {
+                                onclick: function() {
+                                    showModal = false;
+                                    m.redraw();
+                                }
+                            }, 'Cancel')
+                        )
+
                     )
                 )
             );
