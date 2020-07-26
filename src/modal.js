603b03a072b372474ec7885e0e949f311201e679
wilgaboury <wgaboury@gmail.com>
2020-07-26 10:13:12 -0400

    changes

---
+++ src/modal.js
@@ -0,0 +1,15 @@
+function Modal() {
+    return m();
+}
+
+let isOpen = false;
+
+export const ModalContainer = {
+    view: function(vnode) {
+        return m();
+    }
+}
+
+export function openModal(content) {
+    
+}
