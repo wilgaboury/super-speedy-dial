42ef7b73ce5fca333b85688b6850b1d98e2f3d0f
Wil Gaboury <wgaboury@feith.com>
2020-07-29 23:09:42 -0400

    barley working. bout to make some radical changes

--- src/bookmark.js
+++ src/bookmark.js
@@ -6,9 +6,11 @@ function Bookmark() {
     let first;
     let last;
 
+    let domNode;
+
     return {
-        oninit: function(vnode) {
-            console.log(vnode.attrs.bookmarkNode);
+        getBoundingClientRect() {
+            return domNode.getBoundingClientRect();
         },
 
         onbeforeupdate: function(newVnode, oldVnode) {
@@ -22,39 +24,49 @@ function Bookmark() {
         },
 
         view: function(vnode) {
+            domNode = vnode.dom;
             const bookmarkNode = vnode.attrs.bookmarkNode;
             const onmousedown = vnode.attrs.onmousedown;
             const onmouseup = vnode.attrs.onmouseup;
-            const onmouseover = vnode.attrs.onmousover;
+            const onmouseover = vnode.attrs.onmouseover;
+            const onmouseout = vnode.attrs.onmouseout;
+            const left = vnode.attrs.left;
+            const top = vnode.attrs.top;
 
-            return m(".bookmark-container", [
-                m(".bookmark-card", {
-                        onmousedown: function(event) {
-                            isSelected = true;
-                            onmousedown(event, bookmarkNode);
-                            m.redraw();
-                        },
-                        onmouseup: function(event) {
-                            isSelected = false;
-                            onmouseup(event, bookmarkNode);
-                            m.redraw();
-                        },
-                        onmouseover: function(event) {
-                            onmouseover(event, bookmarkNode);
-                            m.redraw();
-                        },
-                        onmouseout: function() {
-                            isSelected = false;
-                            m.redraw();
-                        }
-                    }, [
+            if (!(vnode.attrs.isSelected == null)) {
+                isSelected = vnode.attrs.isSelected;
+            }
+
+            return m(".bookmark-container", {
+                    style: (!(left == null) && !(top == null)) ? `left: ${left}px; top: ${top}px` : '',
+                    onmousedown: function(event) {
+                        isSelected = true;
+                        let rect = vnode.dom.getBoundingClientRect();
+                        onmousedown(event, bookmarkNode, rect.left, rect.top);
+                    },
+                    onmouseup: function(event) {
+                        isSelected = false;
+                        onmouseup(event, bookmarkNode);
+                        m.redraw();
+                    },
+                    onmouseover: function(event) {
+                        onmouseover(event, bookmarkNode);
+                        m.redraw();
+                    },
+                    onmouseout: function(event) {
+                        isSelected = false;
+                        onmouseout(event, bookmarkNode);
+                        m.redraw();
+                    }
+                },
+                m(".bookmark-card", {style: isSelected ? 'border: 2px solid #0390fc' : ''}, [
                         bookmarkNode.type == 'folder' ? 
-                            m('img.folder-image', {src: 'icons/folder.svg', height: '120'}) : 
-                            m('img.website-image', {src: `https://www.google.com/s2/favicons?domain=${encodeURI(bookmarkNode.url)}`, height: '32'})
+                            m('img.folder-image', {src: 'icons/folder.svg', height: '120', draggable: 'false'}) : 
+                            m('img.website-image', {src: `https://www.google.com/s2/favicons?domain=${encodeURI(bookmarkNode.url)}`, height: '32', draggable: 'false'})
                     ]
                 ),
                 m(`.bookmark-title${isSelected ? ' .selected' : ''}`, bookmarkNode.title)
-            ]);
+            );
         }
     }
 };
--- src/grid.js
+++ src/grid.js
@@ -8,9 +8,14 @@ function Grid() {
     let mouseListenerAdded = false;
 
     let isInDragMode = false;
+    let beingDraggedBookmark = null;
     let mouseDownPosLeft = null;
     let mouseDownPosTop = null;
     let hasMouseMovedDuringClick = false;
+    let mousedOverBookmark = null;
+
+    let initLeft = null;
+    let initTop = null;
 
     setTimeout(setGridWidth, 0);
 
@@ -26,52 +31,95 @@ function Grid() {
         onupdate: function(vnode) {
             if (!mouseListenerAdded) {
                 mouseListenerAdded = true;
-                document.querySelector('.grid-container').addEventListener('mousemove', function(event) {
+                document.querySelector('body').addEventListener('mousemove', function(event) {
                     if (isInDragMode) {
                         hasMouseMovedDuringClick = true;
 
-                        let style = document.querySelector('.grid-container > .bookmark-container').style;
-                        style.left = event.offsetX - mouseDownPosLeft;
-                        style.top = eventt.offsetY - mouseDownPosTop;
+                        let elem = document.querySelector('.grid-container > .bookmark-container');
+                        if (elem != null) {
+                            elem.style.left = `${event.pageX - mouseDownPosLeft}px`;
+                            elem.style.top = `${event.pageY - mouseDownPosTop}px`;
+                        }
+
+                        let onMouseUpCallback = function(event) {
+                            isInDragMode = false;
+                            hasMouseMovedDuringClick = false;
+                            m.redraw();
+
+                            document.querySelector('body').removeEventListener('onmouseup', onMouseUpCallback);
+                        }
+                        document.querySelector('body').addEventListener('onmouseup', onMouseUpCallback);
                     }
                 });
             }
         },
 
         view: function(vnode) {
+            let bookmarkMapper = function(bookmark, isBeingDragged = false) {
+                let settings = {
+                    bookmarkNode: bookmark,
+                    onmousedown: function(event, bookmarkNode, left, top) {
+                        console.log('mousedown');
+                        console.log(`X:${event.offsetX}, Y:${event.offsetY}`);
+                        console.log(`Left:${left}, Top:${top}`);
+                        isInDragMode = true;
+                        beingDraggedBookmark = bookmarkNode.id;
+                        mouseDownPosLeft = event.offsetX;
+                        mouseDownPosTop = event.offsetY;
+                        initLeft = left;
+                        initTop = top;
+                    },
+                    onmouseup: function(event, bookmarkNode) {
+                        isInDragMode = false;
+                        hasMouseMovedDuringClick = false;
+
+                        if (!hasMouseMovedDuringClick) {
+                            if (!(bookmarkNode.url == null)) {
+                                window.location.href = bookmarkNode.url;
+                            } else if (bookmarkNode.type == "folder") {
+                                nodeStack.push(bookmarkNode);
+                                m.redraw();
+                            }
+                        }
+                    },
+                    onmouseover: function(event, bookmarkNode) {
+                        // mousedOverBookmark = bookmarkNode.id;
+                    },
+                    onmouseout: function(event, bookmarkNode) {
+                        // mousedOverBookmark = null;
+                    },
+                    isBeingDragged: isBeingDragged
+                };
+
+                if (!isBeingDragged) {
+                    settings.key = bookmark.id;
+                } else {
+                    settings.isSelected = true;
+
+                    settings.left = initLeft - mouseDownPosLeft;
+                    settings.top = initTop - mouseDownPosTop;
+                }
+
+                return m(Bookmark, settings);
+            };
+
             let bookmarkList = [];
             if (nodeStack.length > 0) {
-                bookmarkList = nodeStack[nodeStack.length - 1].children
+                let bookmarkListNotMapped = nodeStack[nodeStack.length - 1].children
                     .filter(bookmark => bookmark.type != 'separator')
                     .filter(bookmark => bookmark.url == null || bookmark.url.substring(0, 6) != "place:")
-                    .map(bookmark => {
-                        return m(Bookmark, {
-                            bookmarkNode: bookmark,
-                            onmousedown: function(event, bookmarkNode) {
-                                isInDragMode = true;
-                            },
-                            onmouseup: function(event, bookmarkNode) {
-                                isInDragMode = false;
+                    .filter(bookmark => !isInDragMode || !hasMouseMovedDuringClick || bookmark.id != beingDraggedBookmark);
 
-                                if (!hasMouseMovedDuringClick) {
-                                    if (!(bookmarkNode.url == null)) {
-                                        window.location.href = bookmarkNode.url;
-                                    } else if (bookmarkNode.type == "folder") {
-                                        nodeStack.push(bookmarkNode);
-                                        m.redraw();
-                                    }
-                                } else {
-                                    hasMouseMovedDuringClick = false;
-                                }
-                            },
-                            onmouseover: function(event, bookmarkNode) {
-
-                            }
-                        });
-                    });
+                for (let i = 0; i < bookmarkListNotMapped.length; i++) {
+                    if (isInDragMode && hasMouseMovedDuringClick && mousedOverBookmark == bookmarkListNotMapped[i].id) {
+                        bookmarkList.push(m('.bookmark-placeholder', {key: beingDraggedBookmark, style: 'width: 240px'}));
+                    }
+                    bookmarkList.push(bookmarkMapper(bookmarkListNotMapped[i]));
+                }
             }
 
             return m('.grid-container',
+                (isInDragMode && hasMouseMovedDuringClick ? nodeStack[nodeStack.length - 1].children.filter(b => b.id == beingDraggedBookmark).map(b => bookmarkMapper(b, true))[0] : m('.empty')),
                 m('.back-button-container', nodeStack.length > 1 ? 
                     m('.back-button.button', { 
                             style: 'font-size: 20px',
