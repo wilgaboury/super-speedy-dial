fff293d77dd0282ded8219adea3a355b99ea2535
wilgaboury <wgaboury@gmail.com>
2020-08-09 21:17:23 -0400

    saving background works now

--- src/background.js
+++ src/background.js
@@ -1,13 +1,19 @@
 import Grid from './grid.js';
 import Modal from './modal.js';
-// import { getIDBObject, setIDBOjbect } from './idb.js';
+import { getIDBObject, setIDBObject } from './idb.js';
 
 function Background() {
     let showModal = false;
     let background = null;
 
+    let chosenFile;
+
     return {
         oninit: function() {
+            getIDBObject('background_store', 'background', function(value) {
+                background = value;
+                m.redraw();
+            });
         },
         view: function() {
             return m('.background', { style: background == null ? '' : `background-image: url(${URL.createObjectURL(background)})`},
@@ -24,16 +30,13 @@ function Background() {
                     m('.modal-content',
                         m('h1.settings-label', 'Settings'),
                         m('h2.settings-label', 'Background Image'),
-                        m('input#background-input', {type: 'file', style: 'margin-bottom: 10px'}),
+                        m('input#background-input', {type: 'file', accept: '.png,.jpg,.jpeg.gif', style: 'margin-bottom: 10px'}),
                         m('.modal-button-container',
                             m('.flex-spacer'),
                             m('.button', {
                                 onclick: function() {
-                                    const file = document.querySelector('#background-input').files[0];
-
-                                    getFileStorage({name: 'wils-storage'}).then(function(storage) {
-                                        storage.put('background', file).then(() => m.redraw());
-                                    });
+                                    chosenFile = document.querySelector('#background-input').files[0];
+                                    setIDBObject("background_store", 'background', chosenFile);
 
                                     showModal = false;
                                     m.redraw();
--- src/idb.js
+++ src/idb.js
@@ -1,22 +1,45 @@
-// const idb = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.OIndexedDB || window.msIndexedDB;
-// const IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.OIDBTransaction || window.msIDBTransaction;
-// const dbVersion = 1.0;
-// const request = idb.open('wils_db', dbVersion);
+const idb = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.OIndexedDB || window.msIndexedDB;
+const IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.OIDBTransaction || window.msIDBTransaction;
+const dbVersion = 1.0;
+const db_request = idb.open('wils_db', dbVersion);
 
-// let db;
+db_request.onupgradeneeded = function(event) {
+    let database = event.target.result;
+    database.createObjectStore("background_store");
+}
 
-// request.onsuccess = function(event) {
-//     db = request.result;
-// };
+let null_db_callbacks = [];
+let db = null;
+db_request.onsuccess = function(event) {
+    db = event.target.result;
+    for (let callback of null_db_callbacks) {
+        callback();
+    }
+};
 
-// //Consult https://developer.mozilla.org/en-US/docs/Web/API/IDBRequest to understand the return value
+//Consult https://developer.mozilla.org/en-US/docs/Web/API/IDBRequest to understand the return value
 
-// export function getIDBObject(store_name, object_name) {
-//     let transaction = db.transaction([store_name], IDBTransaction.READ_WRITE);
-//     return transaction.objectStore(store_name).get(object_name);
-// }
+export function getIDBObject(store_name, object_name, callback) {
+    if (db == null) {
+        null_db_callbacks.push(() => getIDBObject(store_name, object_name, callback));
+    } else {
+        let transaction = db.transaction([store_name], 'readwrite');
+        let request = transaction.objectStore(store_name).get(object_name);
+        request.onsuccess = function(event) {
+            callback(event.target.result);
+        };
+        request.onerror = function(event) {
+            console.log('oh fuck');
+        }
+    }
+}
 
-// export function setIDBObject(store_name, object_name, blob) {
-//     let transaction = db.transaction([store_name], IDBTransaction.READ_WRITE);
-//     return transaction.objectStore(store_name).put(blob, object_name);
-// }
+export function setIDBObject(store_name, object_name, object) {
+    console.log('got here');
+    if (db == null) {
+        null_db_callbacks.push(() => setIDBObject(store_name, object_name, object, callback));
+    } else {
+        let transaction = db.transaction([store_name], 'readwrite');
+        transaction.objectStore(store_name).put(object, object_name);
+    }
+}
