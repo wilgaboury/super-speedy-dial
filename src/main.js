b6518ed90e64d61746acc242963d315a408c2ce8
Wil Gaboury <wgaboury@feith.com>
2020-08-18 10:52:55 -0400

    newest version of muuri

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import { addWindowResizeListener } from './grid_width_hack.js'
 
 // addWindowResizeListener();
 
+Muuri.defaultPacker.destroy();
+Muuri.defaultPacker = new Muuri.Packer(0);
+
 m.mount(document.body, Background);
