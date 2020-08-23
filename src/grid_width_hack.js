e8ef58d9f8ae532860f24b621e2d842161b01408
wilgaboury <wgaboury@gmail.com>
2020-08-23 16:02:32 -0400

    removing old code

--- src/grid_width_hack.js
+++
@@ -1,6 +0,0 @@
-let listenerAdded = false;
-export function addWindowResizeListener() {
-    if (!listenerAdded) {
-        document.querySelector('body').addEventListener('resize', function() { m.redraw(); });
-    }
-}
