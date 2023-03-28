277aea6916226a88bb0f5bf73076138d2d330a48
Wil Gaboury <wgaboury@gmail.com>
2023-03-28 17:08:11 -0400

    Update content security policy so Muuri can run web workers

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,6 @@ import { render } from "solid-js/web";
 import "./index.css";
 import App from "./App";
 import { hashIntegration, Router } from "@solidjs/router";
-import Muuri from "muuri";
-
-// Makes the default muuri layout algorithm run synchronously
-Muuri.defaultPacker.destroy();
-Muuri.defaultPacker = new Muuri.Packer(0);
 
 render(
   () => (
