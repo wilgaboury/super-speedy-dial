350f8db55c095871e16b1997feff57ab1bd763d8
wilgaboury <wgaboury@gmail.com>
2023-05-29 15:44:07 -0400

    Add code for pluging in custom layouts

--- src/drag/drag.tsx
+++ src/drag/drag.tsx
@@ -1,62 +1,57 @@
 import {
   Accessor,
-  Component,
   Context,
   For,
+  JSX,
   createContext,
   createEffect,
   createMemo,
+  createSignal,
+  on,
+  onCleanup,
   onMount,
   untrack,
   useContext,
 } from "solid-js";
-import { Dynamic } from "solid-js/web";
 
-interface DragContextData<T> {
-  readonly data: Accessor<T | undefined>;
-  readonly setData: (data: T | undefined) => void;
-  readonly ref: Accessor<HTMLElement | undefined>;
-  readonly setRef: (ref: HTMLElement | undefined) => void;
+interface DroppableRef {
+  readonly ref: HTMLDivElement;
 }
 
-const defaultDragContextData: DragContextData<any> = {
-  data: () => undefined,
-  setData: () => {},
-  ref: () => undefined,
-  setRef: () => {},
-};
+interface DragContextValue<T> {
+  readonly mountDroppable: (droppable: DroppableRef) => void;
+  readonly cleanupDroppable: (droppable: DroppableRef) => void;
+}
 
-type DragContext<T> = Context<DragContextData<T>>;
+type DragContext<T> = Context<DragContextValue<T>>;
 
 function createDragContext<T>(): DragContext<T> {
-  return createContext<DragContextData<T>>(defaultDragContextData);
+  return createContext<DragContextValue<T>>({} as DragContextValue<T>);
 }
 
-interface DraggableContextData<T> {
+function createDragContextValue<T>(): DragContextValue<T> {
+  const droppables = new Set<DroppableRef>();
+  return {
+    mountDroppable: (ref: DroppableRef) => {
+      droppables.add(ref);
+    },
+    cleanupDroppable: (ref: DroppableRef) => {
+      droppables.delete(ref);
+    },
+  };
+}
+
+interface DraggableProps<T> {
   readonly item: T;
   readonly idx: Accessor<number>;
   readonly containerRef: (el: HTMLElement) => void;
   readonly handleRef: (el: HTMLElement) => void;
 }
 
-type DraggableContext<T> = Context<DraggableContextData<T>>;
+type DraggableContext<T> = Context<DraggableProps<T>>;
 
 function createDraggableContext<T>(): DraggableContext<T> {
-  return createContext<DraggableContextData<T>>(
-    {} as DraggableContextData<T> /* this is purposley wrong */
-  );
-}
-
-interface DraggableProps<T> {}
-
-function createDraggable<T>(
-  context: DragContext<T>
-): Component<DraggableProps<T>> {
-  return (props) => {
-    const contextData = useContext(context);
-
-    return <></>;
-  };
+  return createContext<DraggableProps<T>>({} as DraggableProps<T>);
 }
 
 interface Position {
@@ -74,22 +69,73 @@ interface Rect {
   readonly size: Size;
 }
 
-type Layout = (idx: number) => Position;
+type Layout = {
+  readonly height: string;
+  readonly width: string;
+  readonly pos: (idx: number) => Position;
+};
+type LayoutGenerator = (
+  elem: HTMLDivElement,
+  sizes: ReadonlyArray<Size>
+) => Layout;
 
-interface DroppableProps<T> {
-  readonly layout: Layout;
-  readonly each: ReadonlyArray<T>;
+function getElemDim(elem: HTMLElement): Size {
+  return {
+    width: elem.offsetWidth,
+    height: elem.offsetHeight,
+  };
 }
 
-function createDroppable<T>(
-  context: DragContext<T>,
-  draggableContext: DraggableContext<T>,
-  component: Component
-): Component<DroppableProps<T>> {
-  return (props) => {
-    const contextData = useContext(context);
+interface DroppableProps<T, U extends JSX.Element> {
+  readonly each: ReadonlyArray<T>;
+  readonly generateLayout: LayoutGenerator;
+  readonly getItemDim?: (elem: HTMLElement, item: T) => Size;
+  readonly dragContextType?: DragContext<T>;
+  readonly children: (props: DraggableProps<T>) => U;
+}
 
-    return (
+function Droppable<T, U extends JSX.Element>(props: DroppableProps<T, U>) {
+  const dragContext =
+    props.dragContextType && useContext(props.dragContextType);
+  let ref: HTMLDivElement | undefined;
+
+  let droppableContextData: DroppableRef | undefined;
+  onMount(() => {
+    droppableContextData = {
+      ref: ref!,
+    };
+    if (dragContext != null) {
+      dragContext.mountDroppable(droppableContextData);
+    }
+  });
+  onCleanup(() => {
+    if (dragContext != null) {
+      dragContext.cleanupDroppable(droppableContextData!);
+    }
+  });
+
+  const itemToContainer = new Map<T, Size>();
+  const [sizes, setSizes] = createSignal<ReadonlyArray<Size>>([]);
+  const layout = createMemo(() => props.generateLayout(ref!, sizes()));
+
+  function updateSizes() {
+    const each = untrack(() => props.each);
+    if (each.length == itemToContainer.size) {
+      setSizes(
+        each.map((item) => {
+          const value = itemToContainer.get(item);
+          if (value == null)
+            throw new Error("item size map out of sync with items");
+          return value;
+        })
+      );
+    }
+  }
+
+  createEffect(on(() => props.each, updateSizes, { defer: true }));
+
+  return (
+    <div ref={ref}>
       <For each={props.each}>
         {(item, idx) => {
           let containerRef: HTMLElement | undefined;
@@ -99,12 +145,23 @@ function createDroppable<T>(
             const container = containerRef!;
             const handle = handleRef == null ? container : handleRef;
 
-            const initPos = untrack(() => props.layout(idx()));
+            createEffect(() => {
+              // allows user variables affecting item size to be tracked for relayout
+              const getItemDim =
+                props.getItemDim != null ? props.getItemDim : getElemDim;
+              itemToContainer.set(item, getItemDim(container, item));
+              updateSizes();
+              onCleanup(() => itemToContainer.delete(item));
+            });
+
+            const [selected, setSelected] = createSignal(false); // TODO: change how this works to use drag context
+
+            const initPos = untrack(() => layout().pos(idx()));
             container.style.transform = `translate(${initPos.x}px, ${initPos.y}px)`;
 
             // if any calc position and reactivley apply animation effect
             let anim: Animation | undefined;
-            const pos = createMemo(() => props.layout(idx()));
+            const pos = createMemo(() => layout().pos(idx()));
             createEffect(() => {
               if (!selected()) {
                 container.classList.add("released");
@@ -120,20 +177,14 @@ function createDroppable<T>(
             });
           });
 
-          return (
-            <draggableContext.Provider
-              value={{
-                item,
-                idx,
-                containerRef: (el) => (containerRef = el),
-                handleRef: (el) => (handleRef = el),
-              }}
-            >
-              <Dynamic component={component} />
-            </draggableContext.Provider>
-          );
+          return props.children({
+            item,
+            idx,
+            containerRef: (el) => (containerRef = el),
+            handleRef: (el) => (handleRef = el),
+          });
         }}
       </For>
-    );
-  };
+    </div>
+  );
 }
